0930f33434051a793f7075e488b3c7b721b66d84
sylvie41 <retatssylvie@gmail.com>
2016-10-03 17:41:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+moyenneAge
+==========
+
+A Symfony project created on October 3, 2016, 5:37 pm.
---
+++ src/phpstorm-php.php
@@ -0,0 +1,14 @@
+<?php
+$students = [
+    "Emmanuel"  => 42,
+    "Thierry"   => 51,
+    "Pascal"    => 45,
+    "Eric"      => 48,
+    "Nicolas"   => 19
+];
+
+foreach ($students as $student){
+    $age += $student;
+}
+
+echo $age / 5;
